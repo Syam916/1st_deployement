c28c7d9301a98b27334ff144e8caa65bb21c3b42
Vardandatasciences <vdatasciences@gmail.com>
2024-12-16 16:41:06 +0530

    changed entire aws and created all newly and updated in sectres in github , removed debug true

--- app.py
+++ app.py
@@ -98,6 +98,6 @@ def logout():
     session.pop('username', None)
     return redirect(url_for('login'))
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
 
 
